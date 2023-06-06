908a9658ed87995291818c0b28d475445df354d2
mokhethi-k <mokhethikabelo@gmail.com>
2023-06-06 13:20:22 +0000

    corrected the if block when rhe number is less than 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 last_number = 0
 if number < 0:
-    number = abs(number) % 10
+   last_number = abs(number) % 10
     last_number *= -1
 else:
     last_number = number % 10
