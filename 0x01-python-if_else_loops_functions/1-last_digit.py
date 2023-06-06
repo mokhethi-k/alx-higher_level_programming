8cba8c11605a163f7b5d6c4edfa3a9d7ecafb612
mokhethi-k <mokhethikabelo@gmail.com>
2023-06-06 19:21:57 +0000

    corrected indentation

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 l_number = 0
 if number < 0:
    l_number = abs(number) % 10
-    l_number *= -1
+   l_number *= -1
 else:
     l_number = number % 10
 if l_number > 5:
