f4973340297f4628d5e337233b5f160666d1fce9
mokhethi-k <mokhethikabelo@gmail.com>
2023-06-06 13:24:53 +0000

    correcterd number of characters per line

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_number = 0
+l_number = 0
 if number < 0:
-   last_number = abs(number) % 10
-    last_number *= -1
+   l_number = abs(number) % 10
+    l_number *= -1
 else:
-    last_number = number % 10
-if last_number > 5:
-    print(f"Last digit of {number} is {last_number} and is greater than 5")
-elif last_number == 0:
-    print(f"Last digit of {number} is {last_number} and is 0")
-elif last_number < 6 and last_number != 0:
-    print(f"Last digit of {number} is {last_number} and is less than 6 and not 0")
+    l_number = number % 10
+if l_number > 5:
+    print(f"Last digit of {number} is {l_number} and is greater than 5")
+elif l_number == 0:
+    print(f"Last digit of {number} is {l_number} and is 0")
+elif l_number < 6 and l_number != 0:
+    print(f"Last digit of {number} is {l_number} and is less than 6 and not 0")
