928275f5c43c2a1a8486e85aae8c54520e1e1abd
mokhethi-k <mokhethikabelo@gmail.com>
2023-06-09 02:00:25 +0000

    replaced a . with : on the line which prints one argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if argv_count == 0:
         print("0 arguments.")
     elif argv_count == 1:
-        print("1 argument.")
+        print("1 argument:")
     else:
         print("{} arguments:".format(argv_count))
     for i in range(argv_count):
