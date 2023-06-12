89179607aec5d852ea1e36f6178b825d19fdb0fc
mokhethi-k <mokhethikabelo@gmail.com>
2023-06-12 11:51:00 +0000

    changed the if statements

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,10 @@
  #!/usr/bin/python3
 def no_c(my_string):
-    new = [i for i in my_string if i != 'c' and i != 'C']
-    return ("".join(new))
+    new = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new += i
+        else:
+            continue
+    return new
         
