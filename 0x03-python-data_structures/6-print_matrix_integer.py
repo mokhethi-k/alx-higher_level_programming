51a0e0a43695af479d9a00dc3fe556811952ea7d
mokhethi-k <mokhethikabelo@gmail.com>
2023-06-12 13:07:13 +0000

    printing the matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    r = len(matrix)
+    c = len(matrix[0])
+
+    for i in range(r):
+        for j in range(c):
+            print("{:d}".format(matrix[i][j]), end=" ")
+        print()
