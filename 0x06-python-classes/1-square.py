4d2cf57567d2c53138c3e11bf82d0d65ae0a8204
mokhethi-k <mokhethikabelo@gmail.com>
2023-06-27 19:40:16 +0000

    defining a class with private instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defining a square class with size"""
+
+
+class Square:
+    """The body of the class"""
+
+    def __init__(self, size):
+        """  This method initializes a new square,
+        the argiment size
+        of the square
+        """
+        self.__size = size
