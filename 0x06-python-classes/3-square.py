9f9e83871c353f8c2633ec3d8acf20419536211e
mokhethi-k <mokhethikabelo@gmail.com>
2023-06-27 20:18:53 +0000

    Defining a class that calculate the area of the square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defining a square class"""
+
+
+class Square:
+    """The body of the class"""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return int(self.__size) ** 2
